8ccf333987fe524b8764faee0107d6cd8ab2b0e0
karynhuynh <79120906+karynhuynh@users.noreply.github.com>
2021-04-21 19:32:32 -0700

    Included the github link

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 // import Weather from "./Weather";
 import Search from "./Search";
 import Forecast from "./Forecast";
-import Temperature from './Temperature'
+import Temperature from "./Temperature";
+
 import "./App.css";
 
 function App() {
   return (
-    <div className="weather">
+    <div className="weather mt-5">
       <Search />
       <Temperature />
       <hr />
       <Forecast />
+      <small>
+        <a href="https://github.com/karynhuynh/weather-react">
+          Open-source code
+        </a>{" "}
+        by Karyn Huynh
+      </small>
     </div>
   );
 }
