05100888e198e7a61243fab04c2d4c932adfb94d
karynhuynh <79120906+karynhuynh@users.noreply.github.com>
2021-04-21 18:45:53 -0700

    Updated app js and css files to include weather app skeleton

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import Weather from "./Weather";
+// import Weather from "./Weather";
+import Search from "./Search";
+import Forecast from "./Forecast";
+import Temperature from './Temperature'
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello KARYN</h1>
-        <Weather city="Tokyo" />
-      </header>
+    <div className="weather">
+      <Search />
+      <Temperature />
+      <hr />
+      <Forecast />
     </div>
   );
 }
