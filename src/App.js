eebcc8807a4b9110ba6eb9de47e37fe10f35891b
karynhuynh <79120906+karynhuynh@users.noreply.github.com>
2021-04-25 14:47:29 -0700

    Removed the github link

--- src/App.js
+++ src/App.js
@@ -8,16 +8,6 @@ function App() {
       <div className="container mt-5">
         <Temperature defaultCity="San Francisco" />
       </div>
-      <small className="align-center">
-        <a
-          href="https://github.com/karynhuynh/weather-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-sourced code
-        </a>{" "}
-        by Karyn Huynh
-      </small>
     </div>
   );
 }
